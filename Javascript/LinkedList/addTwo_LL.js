0ada5f5b2ee2d3b96c75c6008a19f2b22e790704
Rohan Jaiswal <rohanr.jaiswal@lntinfotech.com>
2021-12-31 10:18:42 +0530

    31 dec

--- Javascript/LinkedList/addTwo_LL.js
+++ Javascript/LinkedList/addTwo_LL.js
@@ -27,6 +27,8 @@ var addTwoNumbers = function (l1, l2) {
     return temp.next;
 };
 
-// Input: l1 = [2,4,3], l2 = [5,6,4]
-// Output: [7,0,8]
-// Explanation: 342 + 465 = 807.
+/*
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+ */
