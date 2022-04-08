96042349e624338ad0060ecb61271d1d0f423be2
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-04-08 21:23:16 +0530

    8 april

--- Javascript/Heaps/heaps.js
+++ Javascript/Heaps/heaps.js
@@ -1,3 +1,5 @@
+//https://www.digitalocean.com/community/tutorials/js-binary-heaps
+
 function MaxHeap() {
     //to represent heap we use 1-based index array.Null as first element (ignore)
     this.heap = [null];
@@ -79,3 +81,89 @@ hp.delete(); // [ 6, 4, 2, 1, 3 ]
 hp.delete(); // [ 4, 3, 2, 1 ]
 
 hp.print(); // // [ 4, 3, 2, 1 ]
+
+class MinHeap {
+    constructor(data = []) {
+        this.data = data;
+        this.comparator = (a, b) => a - b;
+        this.heapify();
+    }
+
+    heapify() {
+        if (this.size() < 2) return;
+        for (let i = 1; i < this.size(); i++) {
+            this.bubbleUp(i);
+        }
+    }
+
+    peek() {
+        if (this.size() === 0) return null;
+        return this.data[0];
+    }
+
+    add(value) {
+        this.data.push(value);
+        this.bubbleUp(this.size() - 1);
+    }
+
+    poll() {
+        if (this.size() === 0) return null;
+        const result = this.data[0];
+        const last = this.data.pop();
+        if (this.size() !== 0) {
+            this.data[0] = last;
+            this.bubbleDown(0);
+        }
+        return result;
+    }
+
+    bubbleUp(index) {
+        while (index > 0) {
+            const parentIndex = (index - 1) >> 1;
+            if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    bubbleDown(index) {
+        const lastIndex = this.size() - 1;
+        while (true) {
+            const leftIndex = index * 2 + 1;
+            const rightIndex = index * 2 + 2;
+            let findIndex = index;
+            if (
+                leftIndex <= lastIndex &&
+                this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+            ) {
+                findIndex = leftIndex;
+            }
+            if (
+                rightIndex <= lastIndex &&
+                this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+            ) {
+                findIndex = rightIndex;
+            }
+            if (index !== findIndex) {
+                this.swap(index, findIndex);
+                index = findIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    swap(index1, index2) {
+        [this.data[index1], this.data[index2]] = [
+            this.data[index2],
+            this.data[index1],
+        ];
+    }
+
+    size() {
+        return this.data.length;
+    }
+}
---
+++ Javascript/Heaps/kthLargestInStream.js
@@ -0,0 +1,112 @@
+//https://leetcode.com/problems/kth-largest-element-in-a-stream/submissions/
+
+var KthLargest = function (k, nums) {
+    this.k = k;
+    this.minHeap = new MinHeap();
+    for (const i of nums) {
+        this.minHeap.add(i);
+        if (this.minHeap.size() > this.k) {
+            this.minHeap.poll();
+        }
+    }
+};
+
+KthLargest.prototype.add = function (val) {
+    this.minHeap.add(val);
+    if (this.minHeap.size() > this.k) {
+        this.minHeap.poll();
+    }
+    return this.minHeap.peek();
+};
+
+class MinHeap {
+    constructor(data = []) {
+        this.data = data;
+        this.comparator = (a, b) => a - b;
+        this.heapify();
+    }
+
+    heapify() {
+        if (this.size() < 2) return;
+        for (let i = 1; i < this.size(); i++) {
+            this.bubbleUp(i);
+        }
+    }
+
+    peek() {
+        if (this.size() === 0) return null;
+        return this.data[0];
+    }
+
+    add(value) {
+        this.data.push(value);
+        this.bubbleUp(this.size() - 1);
+    }
+
+    poll() {
+        if (this.size() === 0) return null;
+        const result = this.data[0];
+        const last = this.data.pop();
+        if (this.size() !== 0) {
+            this.data[0] = last;
+            this.bubbleDown(0);
+        }
+        return result;
+    }
+
+    bubbleUp(index) {
+        while (index > 0) {
+            const parentIndex = (index - 1) >> 1;
+            if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    bubbleDown(index) {
+        const lastIndex = this.size() - 1;
+        while (true) {
+            const leftIndex = index * 2 + 1;
+            const rightIndex = index * 2 + 2;
+            let findIndex = index;
+            if (
+                leftIndex <= lastIndex &&
+                this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+            ) {
+                findIndex = leftIndex;
+            }
+            if (
+                rightIndex <= lastIndex &&
+                this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+            ) {
+                findIndex = rightIndex;
+            }
+            if (index !== findIndex) {
+                this.swap(index, findIndex);
+                index = findIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    swap(index1, index2) {
+        [this.data[index1], this.data[index2]] = [
+            this.data[index2],
+            this.data[index1],
+        ];
+    }
+
+    size() {
+        return this.data.length;
+    }
+}
+
+// Input
+// ["KthLargest", "add", "add", "add", "add", "add"]
+// [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+// Output
+// [null, 4, 5, 5, 8, 8]
