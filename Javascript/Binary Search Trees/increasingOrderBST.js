3826539c85415c153ec4dd9379b4cd0a6c7805a0
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-04-17 19:54:47 +0530

    17 april

--- Javascript/Binary Search Trees/increasingOrderBST.js
+++ Javascript/Binary Search Trees/increasingOrderBST.js
@@ -24,5 +24,22 @@ var increasingBST = function (root) {
     return bst;
 };
 
+//better approach
+var increasingBST = function(root) {
+    let dummy = new TreeNode()
+    let node = dummy
+    
+    function traverse(root) {
+        if(root == null) return
+        traverse(root.left)
+        node.right = root
+        node = node.right
+        node.left = null
+        traverse(root.right)
+    }
+    traverse(root)
+    return dummy.right
+};
+
 // Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
 // Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
