7b4dcb3b4afb5d864f8a411f675b34f67674ea4c
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2022-06-04 19:47:00 +0530

    4 june

---
+++ Javascript/Matrix/nQueens.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/n-queens/submissions/
+
+var solveNQueens = function(n) {
+    const res = [];
+    backtrack(res, n);
+    return res;
+};
+
+function backtrack(res, n, board = [], r = 0) {
+    if (r === n) {
+        res.push(board.map(c => '.'.repeat(c) + 'Q' + '.'.repeat(n - c - 1)));
+        return;
+    }
+    for (let c = 0; c < n; c++) {
+        if (!board.some((bc, br) => bc === c || bc === c + r - br || bc === c - r + br)) {
+            board.push(c);
+            backtrack(res, n, board, r + 1);
+            board.pop();
+        }
+    }
+}
