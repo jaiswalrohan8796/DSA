d0e097714dd474716ee319875f40dabf901bacb5
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-06-11 19:47:57 +0530

    11 june

---
+++ Javascript/Array/minOperationsToReduceXtoZero.js
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/submissions/
+
+var minOperations = function(nums, x) {
+    let len = nums.length, best = 0
+    for (let i = 1; i < len; i++) nums[i] += nums[i-1]
+    let y = nums[len-1] - x
+    if (y < 0) return -1
+    if (y === 0) return len
+    for (let i = -1, j = l = 0; i < len - best && l <= x; l = nums[++i]) {
+        while (nums[j] - l < y) j++
+        if (nums[j] - l === y) best = Math.max(best, j - i)
+    }
+    return best > 0 ? len - best : -1
+};
+
+
+
+// Input: nums = [1,1,4,2,3], x = 5
+// Output: 2
+// Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
