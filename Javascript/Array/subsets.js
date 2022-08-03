e7aefaa0d5e94f36976c9220373fea770747195d
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2022-08-03 20:34:12 +0530

    3 aug

---
+++ Javascript/Array/subsets.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/subsets/
+
+function subsets(nums) {
+	const powerset = [];
+	generatePowerset([], 0);
+
+	function generatePowerset(path, index) {
+		powerset.push(path);
+		for (let i = index; i < nums.length; i++) {
+			generatePowerset([...path, nums[i]], i + 1);
+		}
+	}
+
+	return powerset;
+}
