afb0b9296ce170f5dcb98cc67d001168292e6a4e
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-08-04 22:35:58 +0530

    3 aug

--- Javascript/Array/integerToRoman.js
+++ Javascript/Array/integerToRoman.js
@@ -13,6 +13,26 @@ var intToRoman = function (num) {
     );
 };
 
+// public static String intToRoman(int num){
+// 	if (num < 1 || num > 3999) return "";
+
+// 	StringBuilder result = new StringBuilder();
+
+// 	String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+// 	int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+// 	int i = 0;
+//             //iterate until the number becomes zero, NO NEED to go until the last element in roman array
+// 	while (num >0) {
+// 		while ( num >= values[i]) {
+// 			num -= values[i];
+// 			result.append(roman[i]);
+// 		}
+// 		i++;
+// 	}
+// 	return result.toString();
+// }
+
 // Input: num = 1994
 // Output: "MCMXCIV"
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
