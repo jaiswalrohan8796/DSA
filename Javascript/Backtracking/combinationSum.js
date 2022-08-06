8ed4568d86c4d909faa40fb1565c0c1a1d952d74
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-08-06 15:05:20 +0530

    6 aug

--- Javascript/Backtracking/combinationSum.js
+++ Javascript/Backtracking/combinationSum.js
@@ -36,3 +36,28 @@ var combinationSum = function (candidates, target) {
 // 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
 // 7 is a candidate, and 7 = 7.
 // These are the only two combinations.
+
+
+
+/**
+ * public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(candidates);
+    backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+    if (remain < 0) return;
+    else if (remain == 0) list.add(new ArrayList<>(tempList));
+    else{
+        for (int i = start; i < cand.length; i++) { 
+            tempList.add(cand[i]);
+            backtrack(list, tempList, cand, remain-cand[i], i);
+            tempList.remove(tempList.size()-1);
+        } 
+    }
+
+}
+ * 
+ */
