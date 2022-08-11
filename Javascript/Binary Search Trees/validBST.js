5d87b20d916464d2a0c00f2def82477d4f0a0c0d
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2022-08-11 17:18:32 +0530

    11 aug

--- Javascript/Binary Search Trees/validBST.js
+++ Javascript/Binary Search Trees/validBST.js
@@ -25,6 +25,19 @@ var isValidBST = function (root) {
     return true;
 };
 
+
+var isValidBST = function(root) {
+    function isBST(root, min, max) {
+        if(!root) return true
+        if(root.val >= max || root.val <= min) return false
+        return isBST(root.left, min, root.val) && isBST(root.right, root.val, max)
+    }
+    
+    return isBST(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+};
+
+
+
 // Input: root = [5,1,4,null,null,3,6]
 // Output: false
 // Explanation: The root node's value is 5 but its right child's value is 4.
