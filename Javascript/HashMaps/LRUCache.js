2b39bf56d4ff08ddc088c15dd81f7fdc37e2a23d
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-08-15 15:28:34 +0530

    15 aug

--- Javascript/HashMaps/LRUCache.js
+++ Javascript/HashMaps/LRUCache.js
@@ -27,3 +27,74 @@ LRUCache.prototype.put = function (key, value) {
 };
 
 //We can also use Double Ended LinkedList to store Least Recent Used & Most Recent Used keys.
+
+function Node(key, value) {
+    this.key = key;
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    push(key, val) {
+        let newNode = new Node(key, val);
+        if (!this.head) {
+            this.head = this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.size++;
+        return newNode;
+    }
+    remove(node) {
+        if (!node.next && !node.prev) {
+            this.head == null;
+            this.tail == null;
+        } else if (!node.next) {
+            this.tail = node.prev;
+            this.tail.next = null;
+        } else if (!node.prev) {
+            this.head = node.next;
+            this.head.prev = null;
+        } else {
+            const prev = node.prev;
+            const next = node.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+        this.size--;
+    }
+}
+
+class LRUCache {
+    constructor(capacity) {
+        this.cap = capacity;
+        this.cache = {};
+        this.DL = new DoublyLinkedList();
+    }
+    get(key) {
+        if (!this.cache[key]) return -1;
+        const val = this.cache[key].val;
+        this.DL.remove(this.cache[key]);
+        this.cache[key] = this.DL.push(key, val);
+        return val;
+    }
+    put(key, val) {
+        if (this.cache[key]) {
+            this.DL.remove(this.cache[key]);
+        }
+        this.DL.push(key, val);
+        if (this.DL.size > this.cap) {
+            let lru = this.DL.head.key;
+            delete this.cache[lru];
+            this.DL.remove(this.DL.head);
+        }
+    }
+}
