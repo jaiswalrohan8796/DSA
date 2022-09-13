f48174617c6b9ff004cc1fb195249d4e12807598
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-09-13 23:26:35 +0530

    13 sept

--- Javascript/LinkedList/mergeTwoSortedList.js
+++ Javascript/LinkedList/mergeTwoSortedList.js
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/merge-two-sorted-lists/submissions/
 
+//Iterative
 var mergeTwoLists = function (list1, list2) {
     let mergeHead = new ListNode(-1);
     let curr = mergeHead;
@@ -17,6 +18,23 @@ var mergeTwoLists = function (list1, list2) {
     return mergeHead.next;
 };
 
+//Recursive
+
+var mergeTwoLists = function (list1, list2) {
+    function merge(m, n) {
+        if (m == null || n == null) {
+            return m || n;
+        }
+        if (m.val <= n.val) {
+            m.next = merge(m.next, n);
+            return m;
+        } else {
+            n.next = merge(m, n.next);
+            return n;
+        }
+    }
+    return merge(list1, list2);
+};
 
 // Input: list1 = [1,2,4], list2 = [1,3,4]
 // Output: [1,1,2,3,4,4]
--- Javascript/LinkedList/reOrderLL.js
+++ Javascript/LinkedList/reOrderLL.js
@@ -1,29 +1,45 @@
 //https://leetcode.com/problems/reorder-list/
 
 var reorderList = function (head) {
-    if (head == null) {
-        return null;
+    if (head == null || head.next == null) return head;
+    //find mid
+    let slow = head;
+    let fast = head;
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    let stack = [];
-    let node = head;
-    while (node) {
-        stack.push(node);
-        node = node.next;
+    //reverse next half
+    let prev = null;
+    let curr = slow.next;
+    while (curr) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
     }
-
-    node = head;
-    const size = stack.length;
-    for (let i = 0; i < size; i++) {
-        if (i % 2 == 0) {
-            node.next = stack.shift();
-        } else {
-            node.next = stack.pop();
-        }
-        node = node.next;
+    slow.next = null;
+    //pick from 1 -> 2 -> 1 -> 2...
+    let head1 = head;
+    let head2 = prev;
+    while (head1 && head2) {
+        let nxt1 = head1.next;
+        let nxt2 = head2.next;
+        //pick from 2
+        head1.next = head2;
+        //update 1
+        head1 = nxt1;
+        //pick from 1
+        head2.next = head1;
+        //update 2
+        head2 = nxt2;
     }
-    node.next = null;
-    return head;
 };
+
+//3-step process
+//find middle
+//reverse right half
+//switch connections alternatively 1 -> 2 -> 1 -> 2...
 
 // Input: head = [1,2,3,4]
 // Output: [1,4,2,3]
