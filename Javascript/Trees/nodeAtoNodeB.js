80fbdedde5dd5e060acb85fd757f33b54c58099f
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2022-09-22 18:33:17 +0530

    22 sept

--- Javascript/Trees/nodeAtoNodeB.js
+++ Javascript/Trees/nodeAtoNodeB.js
@@ -1,35 +1,29 @@
 //https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/
 
-
 var getDirections = function(root, startValue, destValue) {
-    function findPath(node, path, target) {
-        if(!node) return false
-        if(node.val == target) {
-            return true
-        }
-        let left = findPath(node.left, path, target)
-        if(left) {
-            path.unshift("L")
-            return true
-        }
-        let right = findPath(node.right, path, target)
-        if(right) {
-            path.unshift("R")
-            return true
-        }
-        return false
-    }
-    let startPath = []
-    findPath(root, startPath, [startValue])
-    let endPath = []
-    findPath(root, endPath, destValue)
+    let lca = LCA(root, startValue, destValue)
+    let depth = findDepth(lca, startValue)
+    let path = findPath(lca, destValue)
+    return "U".repeat(depth).concat(path)
     
-    while(startPath[0] == endPath[0]) {
-        startPath.shift()
-        endPath.shift()
+    function LCA(node, s, d) {
+        if(!node) return null
+        if(node.val == s || node.val == d) return node
+        let left = LCA(node.left, s ,d)
+        let right = LCA(node.right, s, d)
+        if(left && right) return node
+        return left || right
     }
-    let ans = "U".repeat(startPath.length)
-    ans += endPath.join("")
-    return ans
-}
     
+    function findDepth(node, s, c=0) {
+        if(!node) return 0
+        if(node.val == s) return c
+        return findDepth(node.left, s, c+1) || findDepth(node.right, s, c+1)
+    }
+    
+    function findPath(node, d, str="") {
+        if(!node) return ""
+        if(node.val == d) return str
+        return findPath(node.left, d, str+"L") || findPath(node.right, d, str+"R")
+    }
+};
