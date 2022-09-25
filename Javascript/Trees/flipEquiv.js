f398cda639a8bddb13d7a32cf3a7caa13e7ecc59
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2022-09-25 17:22:06 +0530

    25 sept

---
+++ Javascript/Trees/flipEquiv.js
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/flip-equivalent-binary-trees/submissions/
+
+var flipEquiv = function (root1, root2) {
+    if (root1 == null || root2 == null) return root1 == root2;
+    return (
+        root1.val == root2.val &&
+        ((flipEquiv(root1.left, root2.left) &&
+            flipEquiv(root1.right, root2.right)) ||
+            (flipEquiv(root1.left, root2.right) &&
+                flipEquiv(root1.right, root2.left)))
+    );
+};
