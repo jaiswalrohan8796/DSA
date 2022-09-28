f83379a14fa4b69c090620bcf0ff4f7d7d25052b
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2022-09-28 13:54:40 +0530

    28 sept

--- Javascript/Trees/BTtoLL.js
+++ Javascript/Trees/BTtoLL.js
@@ -1,13 +1,20 @@
 //https://leetcode.com/problems/flatten-binary-tree-to-linked-list/submissions/
 
 var flatten = function(root) {
-    let curr = root
-    while (curr) {
-        if (curr.left) {
-            let runner = curr.left
-            while (runner.right) runner = runner.right
-            runner.right = curr.right, curr.right = curr.left, curr.left = null
+    if(!root) return null
+    let stack = [root]
+    while(stack.length != 0) {
+        let curr = stack.pop()
+        if(curr.right) {
+            stack.push(curr.right)
         }
-        curr = curr.right
+        if(curr.left) {
+            stack.push(curr.left)
+        }
+        if(stack.length != 0) {
+            curr.right = stack[stack.length-1]
+        }
+        curr.left = null
     }
-};
+    return root
+}
