c552a8c0c9193966639a13b0aecbc87d459da15b
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2023-01-16 12:30:15 +0530

    16 Jan

--- Javascript/Array/longestConsecutiveSequence.js
+++ Javascript/Array/longestConsecutiveSequence.js
@@ -1,26 +1,23 @@
 //https://leetcode.com/problems/longest-consecutive-sequence/submissions/
 
-function longestConsecutive(nums) {
-    if (nums == null || nums.length === 0) return 0;
-
-    const set = new Set(nums);
-    let max = 0;
-
-    for (let num of set) {
-        if (set.has(num - 1)) continue; // make sure starting from the beginning of sequence
-
-        let currNum = num;
-        let currMax = 1;
-
-        while (set.has(currNum + 1)) {
-            currNum++;
-            currMax++;
-        }
-        max = Math.max(max, currMax);
+var longestConsecutive = function(nums) {
+    let hmap = new Map()
+    for(let i = 0; i < nums.length; i++) {
+        hmap.set(nums[i], i)
     }
-
-    return max;
-}
+    let maxx = 0
+    for(let i = 0; i < nums.length; i++) {
+        let curr = nums[i]
+        if(hmap.has(curr - 1)) continue
+        let size = 0
+        while(hmap.has(curr)) {
+            size += 1
+            curr += 1
+        }
+        maxx = Math.max(maxx, size)
+    }
+    return maxx
+};
 
 // Input: nums = [100,4,200,1,3,2]
 // Output: 4
