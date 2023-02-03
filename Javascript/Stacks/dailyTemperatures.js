1aa1bfa7e3e9359d99ea05ade33e8971c1b33895
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2023-02-03 19:52:11 +0530

    3 Feb

--- Javascript/Stacks/dailyTemperatures.js
+++ Javascript/Stacks/dailyTemperatures.js
@@ -1,5 +1,20 @@
 //https://leetcode.com/problems/daily-temperatures/submissions/
 
+
+var dailyTemperatures = function(temp) {
+    let res = new Array(temp.length).fill(0)
+    let stack = []
+    for(let i = 0; i < temp.length; i++) {
+        while(stack.length > 0 && temp[i] > temp[stack.at(-1)]) {
+            let id = stack.pop()
+            res[id] = i - id
+        }
+        stack.push(i)
+    }
+    return res
+};
+
+
 var dailyTemperatures = function (temperatures) {
     let ans = [];
     for (let i = 0; i < temperatures.length; i++) {
